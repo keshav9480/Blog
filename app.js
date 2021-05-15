848be0db48cf597a51f0963ccab8efe837c45a03
keshav-n-01 <p.keshav.n@gmail.com>
2021-05-15 21:07:43 +0530

    updated

--- app.js
+++ app.js
@@ -2,6 +2,9 @@ const express = require('express');
 const bodyParser = require('body-parser');
 const ejs = require('ejs')
 var _ = require('lodash');
+var expAlert = require("js-alert");
+var al = require('alert')
+
 
 const homeStartingContent = "Lacus vel facilisis volutpat est velit egestas dui id ornare. Semper auctor neque vitae tempus quam. Sit amet cursus sit amet dictum sit amet justo. Viverra tellus in hac habitasse. Imperdiet proin fermentum leo vel orci porta. Donec ultrices tincidunt arcu non sodales neque sodales ut. Mattis molestie a iaculis at erat pellentesque adipiscing. Magnis dis parturient montes nascetur ridiculus mus mauris vitae ultricies. Adipiscing elit ut aliquam purus sit amet luctus venenatis lectus. Ultrices vitae auctor eu augue ut lectus arcu bibendum at. Odio euismod lacinia at quis risus sed vulputate odio ut. Cursus mattis molestie a iaculis at erat pellentesque adipiscing.";
 const aboutContent = "Hac habitasse platea dictumst vestibulum rhoncus est pellentesque. Dictumst vestibulum rhoncus est pellentesque elit ullamcorper. Non diam phasellus vestibulum lorem sed. Platea dictumst quisque sagittis purus sit. Egestas sed sed risus pretium quam vulputate dignissim suspendisse. Mauris in aliquam sem fringilla. Semper risus in hendrerit gravida rutrum quisque non tellus orci. Amet massa vitae tortor condimentum lacinia quis vel eros. Enim ut tellus elementum sagittis vitae. Mauris ultrices eros in cursus turpis massa tincidunt dui.";
@@ -42,18 +45,18 @@ app.post('/compose', (req, res) => {
 		textDescription : req.body.textDescription,
 	}
 	if(postData.titleName === "" || postData.textDescription === ""){
-		alert('Title Field or Description is none!!');
-		redirect('/compose')
+		al.alert('Title Field or Description is none!!');
+		res.redirect('/compose')
+	}else{
+		articleList.unshift(postData);
+		console.log(articleList);
 	}
-	articleList.unshift(postData);
-	console.log(articleList);
 	 res.redirect('/');
 });
 
 app.get("/posts/:postName", function(req, res){
 
 	console.log(req.params.postName)
-
 	const requestedTitle = _.lowerCase(req.params.postName);
 	articleList.forEach(function(post){
 	  const storedTitle = _.lowerCase(post.titleName);
@@ -64,7 +67,6 @@ app.get("/posts/:postName", function(req, res){
 		});
 	  }
 	});
-  
   });
 
 //launch app
